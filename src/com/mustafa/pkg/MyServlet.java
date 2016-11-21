0b67ff8fdb3c9354e80f455b277e509f8c490468
Mustafa B <Mustafa_B@infosys.com>
2016-11-21 10:59:45 +0530

    Updated the servlet file AGAIn

--- src/com/mustafa/pkg/MyServlet.java
+++ src/com/mustafa/pkg/MyServlet.java
@@ -25,7 +25,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.print("Hello People!! Thanks for the response!!!");
+		out.print("Hello People!! Thanks for this response!!!");
 	}
 
 	/**
