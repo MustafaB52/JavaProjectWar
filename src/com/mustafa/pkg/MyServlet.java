f380cd7f00cd4cb191c7e9bb9ed330a18249c87d
Mustafa B <Mustafa_B@infosys.com>
2016-11-21 10:01:08 +0530

    Updated the servlet file to remove TODO

--- src/com/mustafa/pkg/MyServlet.java
+++ src/com/mustafa/pkg/MyServlet.java
@@ -18,14 +18,12 @@ public class MyServlet extends HttpServlet {
      */
     public MyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.print("Hello People!!");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -35,7 +33,6 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
